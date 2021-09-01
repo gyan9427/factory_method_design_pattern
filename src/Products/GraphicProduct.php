d9b9694d2a26609db6b56263f2da09d5eef5bd88
gyan <gyan9427@gmail.com>
2021-09-01 17:54:57 +0530

    refactor getProperties in GraphicProduct class to include image

--- src/Products/GraphicProduct.php
+++ src/Products/GraphicProduct.php
@@ -10,7 +10,11 @@ class GraphicProduct implements Product
 
     public function getProperties()
     {
-        $this->mfgProduct = "This is a graphic.<3";
+        $this->mfgProduct="<!doctype html><html><head><meta charset='UTF-8' />";
+        $this->mfgProduct.="<title>Map Factory</title>";
+        $this->mfgProduct.="</head><body>";
+        $this->mfgProduct.="<img src='https://cdn.pixabay.com/photo/2021/08/24/15/38/sand-6570980_960_720.jpg' width='500' height='500' />";
+        $this->mfgProduct.="</body></html>"; 
         return $this->mfgProduct;
     }
 }
